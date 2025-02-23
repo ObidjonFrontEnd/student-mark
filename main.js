4c83e38a60159399f1a5e507710b2e752d57e93a
ObidjonFrontEndn <lightline002@gmail.com>
2025-02-23 17:56:03 +0500

    demo v-1.0.001

--- main.js
+++ main.js
@@ -8,14 +8,12 @@ let gradeInput = document.getElementById('ball')
 let forSurNameLabel = document.getElementById('forSurName')
 let forNameLabel = document.getElementById('forName')
 
-
 let teacherBody = document.getElementById('teacherBody')
 let studentBtn = document.getElementById('studentBtn')
 let studentBtn1 = document.getElementById('studentBtn1')
 let teachBtn = document.getElementById('teachBtn')
 let teachBtn1 = document.getElementById('teachBtn1')
 let students = document.getElementById('students')
-
 
 let table = document.getElementById('studentsTable')
 let dataUsers = []
@@ -47,8 +45,11 @@ function creat() {
 		surname: surNameInput.value,
 		date: 0,
 		ball: 0,
-		count:0
+		count: 0,
 	})
+	nameInput.value = ''
+	surNameInput.value = ''
+
 	localStorage.setItem('users', JSON.stringify(dataUsers))
 	read()
 }
@@ -63,7 +64,7 @@ function creat() {
 
 function read() {
 	table.innerHTML = ''
-	dataUsers.map((obj , index) => {
+	dataUsers.map((obj, index) => {
 		table.innerHTML += `
 			<tr class = "mx-auto text-[13px] sm:text-[15px] md:text-[20px]">
 				<td class ="mx-auto text-center p-[10px] bg-[#FDD9B5] border-solid border-black border-[1px]">${obj.name}</td>
@@ -79,44 +80,44 @@ let btn = document.getElementById('btn')
 addBallForm.addEventListener('submit', function (event) {
 	event.preventDefault()
 	let date = new Date()
-	let index = addBallForm.dataset.index;
+	let index = addBallForm.dataset.index
 
-	updata(date , index)
+	updata(date, index)
 	openClose()
 })
-function openClose(index){
+function openClose(index) {
 	addBallForm.classList.toggle('scale-0')
-	addBallForm.dataset.index = index;
+	addBallForm.dataset.index = index
 }
-function updata(date , index){
-	dataUsers.forEach(function(value , key){
-			if(key == index){
-				value.ball  = (value.ball + +gradeInput.value);
-				value.date  = date.toLocaleDateString();
-				localStorage.setItem('users', JSON.stringify(dataUsers))
-				read()
- 			}
+function updata(date, index) {
+	dataUsers.forEach(function (value, key) {
+		if (key == index) {
+			value.ball = value.ball + +gradeInput.value
+			value.date = date.toLocaleDateString()
+			localStorage.setItem('users', JSON.stringify(dataUsers))
+			gradeInput.value = ''
+			read()
+		}
 	})
 }
 
-
-function open(){
+function open() {
 	students.classList.toggle('translate-x-[-100%]')
 	teacherBody.classList.toggle('translate-x-[-100%]')
-	teacherBody.classList.toggle('scale-[0.9]');
-	setTimeout(()=>{
-		students.classList.toggle('scale-[0.9]');	
-	},1000)
+	teacherBody.classList.toggle('scale-[0.9]')
+	setTimeout(() => {
+		students.classList.toggle('scale-[0.9]')
+	}, 1000)
 }
-function closeThis(){
+function closeThis() {
 	teacherBody.classList.toggle('translate-x-[-100%]')
 	teacherBody.classList.toggle('z-20')
 	students.classList.toggle('translate-x-[-100%]')
-	students.classList.toggle('scale-[0.9]');
+	students.classList.toggle('scale-[0.9]')
 
-	setTimeout(()=>{
-		teacherBody.classList.toggle('scale-[0.9]');
-	},1000)
+	setTimeout(() => {
+		teacherBody.classList.toggle('scale-[0.9]')
+	}, 1000)
 }
-studentBtn.addEventListener('click' , open);
-teachBtn1.addEventListener('click' , closeThis);
+studentBtn.addEventListener('click', open)
+teachBtn1.addEventListener('click', closeThis)
